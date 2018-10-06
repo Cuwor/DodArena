86d4811fc52627bc19606292a8c00b7624383096
DESKTOP-6F8Q8M6\Менем <menem8888@gmail.com>
2018-10-06 17:17:17 +0700

    Из-за лупы ИИ просто превосходный

---
+++ Assets/Al_AI/Scripts/Eidolon_Script_Controller.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Eidolon_Script_Controller : MonoBehaviour {
+
+	public GameObject Player;
+	[Space(20)]
+	public NavMeshAgent NavAgent;
+	public float DistanceTP;
+	[Range(1,10)]
+	public float RadiusAttack=2;
+	[Range(90,100)]
+	public float HP =95;
+	// Use this for initialization
+	void Start () {
+		NavAgent = GetComponent<NavMeshAgent>();
+		Player = GameObject.FindWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (HP <= 0)
+		{
+			StartCoroutine(Destroeded());
+		}
+		DistanceTP = Vector3.Distance(Player.transform.position, transform.position);
+		Debug.Log(DistanceTP);
+		if (DistanceTP > RadiusAttack)
+		{
+			NavAgent.enabled = true;
+			NavAgent.destination = Player.transform.position;
+		}
+		else if (DistanceTP<=RadiusAttack)
+		{
+			NavAgent.enabled = false;
+			HP -= 100;
+		}
+		
+	}
+
+	IEnumerator Destroeded()
+	{
+		yield return new WaitForSeconds(2);
+		Destroy(this.gameObject);
+	}
+}
---
+++ Assets/Al_AI/Scripts/Slice_Script_Controller.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class Slice_Script_Controller : MonoBehaviour {
+	[Tooltip("Здесь объект")]
+	[Header("Здесь объект")]
+	public GameObject Player;
+	[Space(20)]
+	public NavMeshAgent NavAgent;
+	public float DistanceTP;
+	[Range(1,10)]
+	public float RadiusAttack;
+	[Range(90,100)]
+	public float HP =95;
+
+	public bool key = true;
+
+	public GameObject[] Eidolons;
+
+	// Use this for initialization
+	void Start ()
+	{
+		NavAgent = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (HP <= 0 && key)
+		{
+			StartCoroutine(CreateEidolons());
+			StartCoroutine(Destroeded());
+			key = false;
+		}
+		DistanceTP = Vector3.Distance(Player.transform.position, transform.position);
+		Debug.Log(DistanceTP);
+		if (DistanceTP > RadiusAttack)
+		{
+			NavAgent.enabled = true;
+			NavAgent.destination = Player.transform.position;
+		}
+		else if (DistanceTP<=RadiusAttack)
+		{
+			NavAgent.enabled = false;
+			HP -= 100;
+		}
+
+	}
+
+	IEnumerator CreateEidolons()
+	{
+		yield return new WaitForSeconds(1f);
+		Instantiate(Eidolons[0], transform.position, Quaternion.identity);
+		Instantiate(Eidolons[0], transform.position, Quaternion.identity);
+		Instantiate(Eidolons[0], transform.position, Quaternion.identity);
+		Instantiate(Eidolons[0], transform.position, Quaternion.identity);
+
+	}
+	IEnumerator Destroeded()
+	{
+		yield return new WaitForSeconds(2);
+		Destroy(this.gameObject);
+	}
+}
+
