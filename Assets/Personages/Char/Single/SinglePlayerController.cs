442e05f15a52081d2d0b15012b086b601c194a4b
DESKTOP-6F8Q8M6\Менем <menem8888@gmail.com>
2018-10-25 18:07:46 +0700

    Pered otkatom

---
+++ Assets/Personages/Char/Single/SinglePlayerController.cs
@@ -0,0 +1,329 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class SinglePLayerController : MyTools, IAlive
+{
+    public PhotonView photonView;
+    public GameObject plCam;
+    private GameObject sceneCam;
+    private Vector3 selfPos;
+    [Tooltip("The local player instance. Use this to know if the local player is represented in the Scene")]
+    public static GameObject LocalPlayerInstance;
+
+    private void Awake()
+    {
+        if (photonView.isMine)
+        {
+            sceneCam = GameObject.Find("Main Camera");
+            sceneCam.SetActive(false);
+            plCam.SetActive(true);
+        }
+        // #Important
+        // used in GameManager.cs: we keep track of the localPlayer instance to prevent instanciation when levels are synchronized
+        if (photonView.isMine)
+        {
+            LocalPlayerInstance = gameObject;
+        }
+
+        // #Critical
+        // we flag as don't destroy on load so that instance survives level synchronization, thus giving a seamless experience when levels load.
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public float Health
+    {
+        get { return health.value; }
+
+        set
+        {
+            if (value <= 0)
+            {
+                Death();
+
+                health.value = 0;
+            }
+
+            health.value = value;
+        }
+    }
+
+    [Tooltip("Подвижная часть тела (следует за камерой)")]
+    public GameObject body;
+
+    [Space(10)] [Range(15, 30)] [Tooltip("Скорость перемещения")]
+    public float speed;
+
+    [Tooltip("Скорость поворота по горизонтали")]
+    public float xSpeed;
+
+    [Tooltip("Скорость поворота по вертикали")]
+    public float ySpeed;
+
+    [Space(10)] [Tooltip("Используемое в данный момент оружие")]
+    public Weapon weapon;
+
+    [Space(10)] [Header("Гравитация")] [Tooltip("Ускорение")]
+    public float grav;
+
+    [Tooltip("Сила прыжка")] public float jumpSpeed;
+
+    [Space(20)] [Header("Части интерфейса")] [Tooltip("Количество патронов")]
+    public Text ammunitionCount;
+
+    [Tooltip("Слайдер для здоровья")] public Slider health;
+
+    [HideInInspector] public bool inDialog;
+
+
+    private Animator anim;
+    private CharacterController controller;
+    private Vector3 gravVector;
+    private Vector3 moveVector;
+    private RecoilRotation view;
+    private const float minY = -100, maxY = 70;
+    private float rotationX, rotationY;
+    private float movementMultiplicator;
+    private float vertSpeed;
+    private bool recoil;
+    private bool reload;
+
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        anim = GetComponent<Animator>();
+        controller = GetComponent<CharacterController>();
+        gravVector = Vector3.down;
+        movementMultiplicator = speed;
+        recoil = false;
+        ammunitionCount.text = "2/0";
+        view = new RecoilRotation();
+        GetComponent<PlayerUI>().pc = this;
+        Health = 100;
+    }
+
+    private void Update()
+    {
+        if (!inDialog)
+        {
+            if (photonView.isMine)
+            {
+                Move();
+                MaxSpeed();
+                Attack();
+                Reload();
+            }
+            else
+            {
+                  SmoothNetMovement();
+            }
+            
+        }
+    }
+
+    void SmoothNetMovement()
+    {
+          transform.position = Vector3.Lerp(transform.position, selfPos, Time.deltaTime*8);
+    }
+
+    private void LateUpdate()
+    {
+        if (!inDialog)
+        {
+            Rotate();
+        }
+
+        if (recoil)
+        {
+            StartCoroutine("Recoil");
+        }
+    }
+
+    #region Показатели
+
+    public void GetDamage(float value)
+    {
+    }
+
+    public void PlusHealth(float value)
+    {
+    }
+
+    public void Death()
+    {
+    }
+
+    #endregion
+
+    #region Перемещение
+
+    private void Move()
+    {
+        if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
+        {
+            var x = Input.GetAxis("Horizontal");
+            var z = Input.GetAxis("Vertical");
+            moveVector = transform.right * x + transform.forward * z;
+        }
+
+        if (controller.isGrounded)
+        {
+            vertSpeed = 0;
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                vertSpeed = jumpSpeed;
+            }
+        }
+
+        vertSpeed += grav * Time.deltaTime;
+        moveVector = new Vector3(moveVector.x * speed * movementMultiplicator * Time.fixedDeltaTime,
+            vertSpeed * Time.deltaTime, moveVector.z * speed * movementMultiplicator * Time.fixedDeltaTime);
+        if (moveVector != Vector3.zero)
+        {
+            controller.Move(moveVector);
+        }
+    }
+
+    private void Rotate()
+    {
+        if (Input.GetAxis("Mouse X") != 0 || Input.GetAxis("Mouse Y") != 0)
+        {
+            var h = Input.GetAxis("Mouse X");
+            var v = Input.GetAxis("Mouse Y");
+            rotationX = transform.localEulerAngles.y + h * xSpeed;
+            rotationY += v * ySpeed;
+            rotationY = Mathf.Clamp(rotationY, minY, maxY);
+            transform.localEulerAngles = new Vector3(0, rotationX, 0);
+            body.transform.localEulerAngles = new Vector3(-rotationY, 0, 0);
+        }
+    }
+
+    private void MaxSpeed()
+    {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            movementMultiplicator = 2;
+        }
+        else
+        {
+            movementMultiplicator = 1;
+        }
+    }
+
+    #endregion
+
+    #region Атака
+
+    private void Attack()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (weapon.MakeShoot())
+            {
+                DrawAmmo();
+                Invoke("AttackEffect", 0.02f);
+            }
+        }
+    }
+
+    private void AttackEffect()
+    {
+        GetRecoilVector(weapon.backForce);
+        recoil = true;
+    }
+
+    private void RotateToView(float force, Vector2 forceVector)
+    {
+        float x, y;
+        x = transform.localEulerAngles.y + force / 2 * Mathf.Sign(forceVector.x - rotationX) * Time.deltaTime;
+        y = force * 3 * Mathf.Sign(forceVector.y - rotationY) * Time.deltaTime;
+        rotationX = x;
+        rotationY += y;
+        rotationY = Mathf.Clamp(rotationY, minY, maxY);
+        transform.localEulerAngles = new Vector3(0, rotationX, 0);
+        body.transform.localEulerAngles = new Vector3(-rotationY, 0, 0);
+    }
+
+    private IEnumerator Recoil()
+    {
+        float force = weapon.backForce;
+        for (float i = force; i > 0; i -= 20)
+        {
+            RotateToView(i / 4, view.newRotation);
+            yield return new WaitForSeconds(0.025f);
+        }
+
+        for (int i = 0; i < force; i++)
+        {
+            RotateToView(0.5f, view.oldRotation);
+        }
+
+        recoil = false;
+    }
+
+    private void GetRecoilVector(float weaponRecoilForce)
+    {
+        view.oldRotation = new Vector2(rotationX, rotationY);
+        int[] c = {-1, 1};
+        view.newRotation = new Vector2(rotationX + c[Random.Range(0, 2)] * weaponRecoilForce / 10,
+            rotationY + weaponRecoilForce);
+    }
+
+    private void Reload()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            weapon.Reload();
+            Invoke("DrawAmmo", weapon.reloadTime);
+        }
+    }
+
+    #endregion
+
+    #region Интерфйс
+
+    public void DrawAmmo()
+    {
+        ammunitionCount.text = weapon.magazin.ToString() + "/" + weapon.ammo.ToString();
+    }
+
+    #endregion
+
+    #region Реакции
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Ammunition amun;
+        if (MyGetComponent(out amun, other.gameObject))
+        {
+            amun.target = gameObject;
+            amun.PC = this;
+            amun.move = true;
+        }
+
+
+        AttackArea at;
+        if (MyGetComponent(out at, other.gameObject))
+        {
+            Health -= at.Damage;
+        }
+    }
+
+    #endregion
+
+    private void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.isWriting)
+        {
+            stream.SendNext(transform.position);
+        }
+        else
+        {
+            selfPos = (Vector3) stream.ReceiveNext();
+        }
+    }
+    
+}
