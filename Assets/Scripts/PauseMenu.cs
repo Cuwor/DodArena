bcd459d615563d6cd8a677436ae7a0e788164c11
Толстиков Артем <temichit@yandex.ru>
2018-11-01 16:08:25 +0700

    Based: Main Menu + Pause Menu

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> _ui;
+
+    void Start()
+    {
+        ResetUi();
+        ToMenu();
+    }
+
+    public void BackToTheGame()
+    {
+        ResetUi();
+    }
+
+    public void ToMenu()
+    {
+        ResetUi();
+        _ui[0].SetActive(true);
+    }
+
+    public void SettingsActive()
+    {
+        ResetUi();
+        _ui[1].SetActive(true);
+    }
+
+    public void QuitApp()
+    {
+        Application.Quit();
+    }
+
+
+    private void ResetUi()
+    {
+        foreach (var el in _ui)
+        {
+            el.SetActive(false);
+        }
+    }
+}
