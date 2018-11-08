182be116ab13f97479260894c29648abf85548e1
Толстиков Артем <temichit@yandex.ru>
2018-11-08 16:02:22 +0700

    WaveSpawners

---
+++ Assets/Scripts/SpawnerWave.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerWave : MonoBehaviour
+{
+    public List<GameObject> EnemysToSpawn;
+    public WaveManager WaveManager;
+    public bool key = false;
+    private int i = 0;
+
+    private void Awake()
+    {
+        WaveManager = GameObject.Find("WaveManager").GetComponent<WaveManager>();
+    }
+
+    private void Update()
+    {
+        if (key && i < EnemysToSpawn.Count)
+        {
+            SpawnEnemy(EnemysToSpawn[i], this.transform.position);
+            i++;
+            
+        }
+    }
+    public void SpawnEnemy(GameObject enemy, Vector3 pos)
+    {
+        if (enemy != null)
+        {
+            var temp = Instantiate(enemy, pos, Quaternion.identity);
+            WaveManager.AddEnemy(temp);
+        }
+        key = false;
+    }
+}
---
+++ Assets/Scripts/WaveManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveManager : MonoBehaviour
+{
+    public bool SpawnKey = false;
+    public List<GameObject> Spawners;
+    public List<GameObject> enemies;
+
+    private void Update()
+    {
+        if (SpawnKey)
+        {
+            foreach (var spawner in Spawners)
+            {
+                spawner.GetComponent<SpawnerWave>().key = true;
+            }
+
+            SpawnKey = false;
+        }
+    }
+
+    public void AddEnemy(GameObject enemy)
+    {
+        enemies.Add(enemy);
+    }
+}
